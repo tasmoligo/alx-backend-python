43fab734319cdb5be905cdeffc2bcd4ef499c07f
Tasie Ibiam <tasieibiam28@gmail.com>
2024-10-10 18:22:44 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' type-annotated function a string str1 and a string str2 as arguments. '''
+
+
+def concat(str1: str, str2: str) -> str:
+    """concats two strings"""
+    return (str1 + str2)
